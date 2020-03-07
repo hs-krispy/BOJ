a6c40b43d189398d8710c0bc3af3b184dcf0413f
hs-krispy <58063806+hs-krispy@users.noreply.github.com>
2020-03-08 01:06:29 +0900

    2565번 #LIS

--- Electric wire.cpp
+++ Electric wire.cpp
@@ -10,7 +10,7 @@ vector<unsigned int> d(100, 1);
 int main()
 {
     int i, j,n,ans=0,res=0;
-    scanf("%d", &n);
+    scanf("%d", &n); 
     for(i=0;i<n;i++)
         scanf("%d %d", &s[i].first, &s[i].second);
     sort(s.begin(), s.end());
