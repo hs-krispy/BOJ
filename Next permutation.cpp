eda012d749c50c7b51f040ef79b0f8b87d7e7cd5
hs-krispy <0864hs@naver.com>
2020-08-07 23:30:36 +0900

    10972번 #순열

---
+++ Next permutation.cpp
@@ -0,0 +1,20 @@
+//
+// Created by 0864h on 2020-08-07.
+//
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int main() {
+    int N;
+    cin >> N;
+    vector<int> v(N);
+    for (int i=0; i < N; i++)
+        cin >> v[i];
+    if (next_permutation(v.begin(), v.end())) {
+        for (int i = 0; i < N; i++)
+            cout << v[i] << ' ';
+    } else
+        cout << "-1";
+    return 0;
+}
