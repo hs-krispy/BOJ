bfba4ef5e8ba0c319462159c7cb9eebe175f5346
hs-krispy <0864hs@naver.com>
2020-10-30 23:48:41 +0900

    1932번 #다이나믹 프로그래밍

--- integer triangle.cpp
+++ integer triangle.cpp
@@ -25,3 +25,29 @@ int main()
     ans = *max_element(&d[n][1], &d[n][n+1]);
     cout<<ans;
 }
+/*
+ -- another solution
+int solution(vector<vector<int>> triangle) {
+    int answer = 0;
+    int length = triangle.size();
+    int d[length][length];
+    for(int i = 1; i < length; i++)
+    {
+        for(int j = 0; j < i + 1; j++)
+        {
+            if(j == 0)
+                triangle[i][j] += triangle[i - 1][j];
+            else if(j == i)
+                triangle[i][j] += triangle[i - 1][j - 1];
+            else
+                triangle[i][j] += max(triangle[i - 1][j], triangle[i - 1][j - 1]);
+            if(i == length - 1)
+            {
+                if(answer < triangle[i][j])
+                    answer = triangle[i][j];
+            }
+        }
+    }
+    return answer;
+}
+ */
