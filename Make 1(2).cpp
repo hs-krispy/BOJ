60921938c551f7f5e98b0ffc884feeaf6394c36f
hs-krispy <0864hs@naver.com>
2021-02-27 01:19:39 +0900

    12852번 #다이나믹 프로그래밍

--- Make 1(2).cpp
+++ Make 1(2).cpp
@@ -1,6 +1,3 @@
-//
-// Created by 0864h on 2020-03-27.
-//
 #include<cstdio>
 using namespace std;
 int d[1000001][2];
@@ -8,31 +5,28 @@ int main()
 {
     int n,i,temp;
     scanf("%d", &n);
-    for(i=2;i<=n;i++)
+    for(i = 2; i <= n; i++)
     {
-        d[i][0] = d[i-1][0]+1;
-        d[i][1] = i-1;
-        if(i%2==0 && d[i][0] >= d[i/2][0]+1)
+        d[i][0] = d[i - 1][0] + 1;
+        d[i][1] = i - 1;
+        if(i % 2 == 0 && d[i][0] >= d[i / 2][0] + 1)
         {
-            d[i][0] = d[i/2][0]+1;
-            d[i][1] = i/2;
+            d[i][0] = d[i / 2][0] + 1;
+            d[i][1] = i / 2;
         }
-        if(i%3==0 && d[i][0] >= d[i/3][0]+1)
+        if(i % 3 == 0 && d[i][0] >= d[i / 3][0] + 1)
         {
-            d[i][0] = d[i/3][0]+1;
-            d[i][1] = i/3;
+            d[i][0] = d[i / 3][0] + 1;
+            d[i][1] = i / 3;
         }
     }
     printf("%d\n", d[n][0]);
     printf("%d ", n);
-    temp = d[n][1];
-    for(i=n-1;i>=2;i--)
+    while(true)
     {
-        if(temp==i)
-        {
-            temp = d[i][1];
-            printf("%d ", i);
-        }
+        if(n == 1)
+            break;
+        printf("%d ", d[n][1]);
+        n = d[n][1];
     }
-    printf("%d", 1);
 }
